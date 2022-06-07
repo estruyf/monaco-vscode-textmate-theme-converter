f0385ca4e0ffdcf489c6a075908053f26f4ae273
Elio Struyf <elio@struyfconsulting.be>
2022-06-07 15:35:05 +0200

    Update umd

--- lib/umd/index.d.ts
+++ lib/umd/index.d.ts
@@ -3,4 +3,15 @@ import * as monaco from 'monaco-editor';
 export * from './interfaces';
 export declare function convertThemeFromFilePath(inputFilePath: string, outputFilePath: string): void;
 export declare function convertThemeFromDir(inputDir: string, outDir: string): void;
+/**
+ * Convert a textmate theme to a Monaco theme
+ * @param theme
+ * @returns
+ */
 export declare function convertTheme(theme: IVSCodeTheme): monaco.editor.IStandaloneThemeData;
+/**
+ * Update colors for the Monaco theme
+ * @param value
+ * @returns
+ */
+export declare const updateColor: (value: string) => string;
